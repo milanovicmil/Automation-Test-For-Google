0df8e0df593a05af6383632666517a8bb3733f6a
Jovana <jovanatester@gmail.com>
2019-02-19 16:11:56 +0100

    add class for firefox webdriver

--- AutomaticTestsForGoogle/src/main/java/com/mycompany/automatictestsforgoogle/util/InitWebDriver.java
+++ AutomaticTestsForGoogle/src/main/java/com/mycompany/automatictestsforgoogle/util/InitWebDriver.java
@@ -1,9 +1,35 @@
 package com.mycompany.automatictestsforgoogle.util;
 
-/**
- *
- * @author milica.milanovic
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+
 public class InitWebDriver {
     
+    WebDriver webDriver;
+    
+    public InitWebDriver(){
+    }
+    
+    public WebDriver initializeWebDriver(){
+        System.setProperty("webdriver.gecko.driver",
+            "C:\\Users\\milica.milanovic\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setCapability("webdriver_accept_untrusted_certs", true);
+        webDriver = new FirefoxDriver(firefoxOptions);
+        webDriver.manage().window().maximize();
+        webDriver.get("https://www.google.rs");   
+       return webDriver;
+  }
+    
+    public void quitWebDriver(WebDriver webDriver){
+        webDriver.quit();
+    }
+    
+    public static void main(String[] args) {
+        InitWebDriver init = new InitWebDriver();
+        WebDriver testDriver = init.initializeWebDriver();
+        init.quitWebDriver(testDriver);
+    }
 }
